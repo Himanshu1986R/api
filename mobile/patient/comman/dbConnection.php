aa4ddb5d180218b30030640bf913008c8db5d847
negiyogi <negi.yogender98@gmail.com>
2025-09-11 18:05:40 +0530

    `Refactored dbConnection.php and updated home.php with new layout codes and image URLs.`

--- mobile/patient/comman/dbConnection.php
+++ mobile/patient/comman/dbConnection.php
@@ -1,12 +1,22 @@
 <?php
 $servername = "localhost";
-$username = "shrchospital_cjroot";
-$password = "CopperjamSarvodaya";
+$username = "root";
+$password = "";
 $limit=20;
 $con = new mysqli($servername, $username, $password,"shrchospital_sarvodaya_db");
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
+
+// echo 'connected';
+// $servername = "119.18.55.181";
+// $username = "shrchospital_cjroot";
+// $password = "CopperjamSarvodaya";
+// $limit=20;
+// $con = new mysqli($servername, $username, $password,"shrchospital_sarvodaya_db");
+// if ($con->connect_error) {
+//     die("Connection failed: " . $con->connect_error);
+// }
 
 
 
